f963592b9d77e7c40f1d9c3c9149968555c4c2ae
iwfan <iwfan@duck.com>
2025-03-19 15:44:21 +0800

    feat: update name

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# React Boilerplate(without Server Component)
+# LLM friendly React Boilerplate(without Server Component)
 
+## Tech Stacks
 
+* [Vite]
+* [Vitest](https://vitest.dev/)
+* [ESLint]
+* [Prettier]
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(async ({ mode }) => {
+  const isProduction = mode === "production";
+  return {
+    plugins: [
+      react(),
+      // put it the last one
+      visualizer({
+        open: isProduction,
+      }),
+    ],
+  };
+});
